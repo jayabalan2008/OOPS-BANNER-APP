0f7ddcdb5aa0ab9cd4673ec51e08c182e8f8d04f
jayabalan2008 <jayabalanelangovan156@gmail.com>
2026-02-24 15:40:23 +0530

    use case 1 print oops

---
+++ OopsBannerApp.java
@@ -0,0 +1,11 @@
+public class OopsBannerApp {
+    public static void main(String[] args) {
+        System.out.println(" *** " + " " + " *** " + " " + "**** " + " " + " ****");
+        System.out.println("** **" + " " + "** **" + " " + "** **" + " " + "**   ");
+        System.out.println("** **" + " " + "** **" + " " + "** **" + " " + "**   ");
+        System.out.println("** **" + " " + "** **" + " " + "**** " + " " + " ****");
+        System.out.println("** **" + " " + "** **" + " " + "**   " + " " + "   **");
+        System.out.println("** **" + " " + "** **" + " " + "**   " + " " + "   **");
+        System.out.println(" *** " + " " + " *** " + " " + "**   " + " " + " ****");
+    }
+}
